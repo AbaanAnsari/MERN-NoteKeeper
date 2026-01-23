60863456705f98b270d56d4fefde387355ba0c07
AbaanAnsari <abaanansari1427@gmail.com>
2026-01-23 13:03:30 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+
+import { Routes, Route } from "react-router"
+
+import HomePage from "./pages/HomePage.jsx"
+import CreatePage from "./pages/CreatePage.jsx"
+import YourNotesDetail from "./pages/YourNotesDetail.jsx"
+import DeleteConfirm from "./components/DeleteConfirm.jsx"
+import useSystemTheme from "./lib/theme.js";
+
+const App = () => {
+
+  useSystemTheme();
+
+  return (
+    <div>
+      <Routes>
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<YourNotesDetail />} />
+        <Route path="/confirm/delete-note/:id" element={<DeleteConfirm />} />
+
+      </Routes>
+
+    </div>
+  );
+};
+
+export default App
